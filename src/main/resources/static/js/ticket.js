308c44ab0806b867471bfc627c32325c467a428f
Stanislav <stasn@ua.fn>
2018-09-21 23:08:19 +0300

    fix map elements add event onButton click

--- src/main/resources/static/js/ticket.js
+++ src/main/resources/static/js/ticket.js
@@ -1,25 +1,23 @@
-
 $('#editOrCreate').on('shown.bs.modal', function (e) {
     resetValidation();
-    group = new H.map.Group();
     $.ajax({
-                            'type': 'get',
-                            'url': basicUrl + "/countryAndCity",
-                            'contentType': 'application/json',
-                            'success': function(data, status){
-                                $.each(data.countryAndCity.Ukraine, function(iter, city){
-                                    $('#city').append($("<option></option>").attr("value",city)
-                                            .text(city));
-                                    });
-                                $.each(data.cars, function(iter, car){
-                                    $('#car').append($("<option></option>").attr("value",car.id)
-                                        .text(car.number + " " + car.producer + ", " + car.model));
-                                      });
-                            },
-                            'error': function (xhr, status) {
-                                alert(xhr.responseJSON.message);
-                            }
-                     });
+        'type': 'get',
+        'url': basicUrl + "/countryAndCity",
+        'contentType': 'application/json',
+        'success': function (data, status) {
+            $.each(data.countryAndCity.Ukraine, function (iter, city) {
+                $('#city').append($("<option></option>").attr("value", city)
+                    .text(city));
+            });
+            $.each(data.cars, function (iter, car) {
+                $('#car').append($("<option></option>").attr("value", car.id)
+                    .text(car.number + " " + car.producer + ", " + car.model));
+            });
+        },
+        'error': function (xhr, status) {
+            alert(xhr.responseJSON.message);
+        }
+    });
 
 
 })
@@ -28,15 +26,17 @@ function saveOrEditTicket() {
 
 }
 
-function redirect(url){
-    $(document).ready( function() {$( location ).attr("href", url);});
+function redirect(url) {
+    $(document).ready(function () {
+        $(location).attr("href", url);
+    });
 }
 
 function addMarkerToGroup(group, coordinate, html) {
-  var marker = new H.map.Marker(coordinate);
-  // add custom data to the marker
-  marker.setData(html);
-  group.addObject(marker);
+    var marker = new H.map.Marker(coordinate);
+    // add custom data to the marker
+    marker.setData(html);
+    group.addObject(marker);
 }
 
 /**
@@ -46,55 +46,64 @@ function addMarkerToGroup(group, coordinate, html) {
  */
 function addInfoBubble(map, coordinates, html) {
 
-  // add 'tap' event listener, that opens info bubble, to the group
-  group.addEventListener('tap', function (evt) {
-    // event target is the marker itself, group is a parent event target
-    // for all objects that it contains
-    var bubble =  new H.ui.InfoBubble(evt.target.getPosition(), {
-      // read custom data
-      content: evt.target.getData()
-    });
-    // show info bubble
-    ui.addBubble(bubble);
-  }, false);
+    // add 'tap' event listener, that opens info bubble, to the group
+    group.addEventListener('tap', function (evt) {
+        // event target is the marker itself, group is a parent event target
+        // for all objects that it contains
+        var bubble = new H.ui.InfoBubble(evt.target.getPosition(), {
+            // read custom data
+            content: evt.target.getData()
+        });
+        ui.getBubbles().forEach(function (bubble) {
+            ui.removeBubble(bubble);
+        });
+        // show info bubble
+        ui.addBubble(bubble);
+    }, false);
 
-  $.each(coordinates, function(key, value){
-      addMarkerToGroup(group,{lat:value.lat, lng:value.lng}, html[key]);
+    $.each(coordinates, function (key, value) {
+        addMarkerToGroup(group, {lat: value.lat, lng: value.lng}, html[key]);
     });
 }
 
 $('#googleMap').on('shown.bs.modal', function (e) {
+    group.getObjects().forEach( function (object) {
+        group.removeObject(object);
+    });
+    ui.getBubbles().forEach(function (bubble) {
+        ui.removeBubble(bubble);
+    });
     $.ajax({
-            'type': 'get',
-            'url': basicUrl + "/placeByCity",
-            'contentType': 'application/json',
-            'data':{
-                city: $("#city").val()
-            },
-            'success': function(data, status){
-                showMap(data);
-            },
-            'error': function (xhr, status) {
-                alert(xhr.responseJSON.message);
-            }
-     });
+        'type': 'get',
+        'url': basicUrl + "/placeByCity",
+        'contentType': 'application/json',
+        'data': {
+            city: $("#city").val()
+        },
+        'success': function (data, status) {
+            showMap(data);
+        },
+        'error': function (xhr, status) {
+            alert(xhr.responseJSON.message);
+        }
+    });
 })
 
-function showMap(places){
+function showMap(places) {
     var coord = [];
     var text = [];
-
-    $.each(places, function(key, value){
-        coord.push({lat:value.lat, lng:value.lng});
-        text.push('<button type="submit" class="btn btn-default btnMapSubmit" data-dismiss="modal" id="' + value.id+'">'
-        + value.street+ ', ' + value.streetNumber +
-        '</button>');
+    $.each(places, function (key, value) {
+        coord.push({lat: value.lat, lng: value.lng});
+        text.push('<button type="button" class="btn btn-default btnMapSubmit" id="' + value.id + '">'
+            + value.street + ', ' + value.streetNumber +
+            '</button>');
     });
-    console.log(coord, map);
-    addInfoBubble(map,coord,text);
     map.getViewPort().resize();
+    addInfoBubble(map, coord, text);
 }
-$('.btnMapSubmit').on('click', function (e){
-    console.log(this.text);
-    $("#address").val(this.text);
+
+$(document).on('click','.btnMapSubmit', function (e) {
+    console.log(e.target.innerText);
+    $("#googleMap").modal('hide');
+    $("#address").val(e.target.innerText);
 })
--- src/main/resources/static/js/validateTicketForm.js
+++ src/main/resources/static/js/validateTicketForm.js
@@ -3,141 +3,149 @@ var map;
 var ui;
 var behavior;
 var group;
-$(document).ready( function () {
+$(document).ready(function () {
     var table = $('#ticketTable').DataTable(
-     {
-           "ajax": {
-                       "url": basicUrl+'/all',
-                       "dataSrc": ""
-                   },
+        {
+            "ajax": {
+                "url": basicUrl + '/all',
+                "dataSrc": ""
+            },
             "columns": [
-                        {"data": "id",
-                         "visible":false },
-                        {title: "Car Model",
-                        "data": null,
-                        render: function ( data, type, row ) {
-                            return data.car.producer + ' ' + data.car.model + ' ' + data.car.number;
-                            }
-                         },
-                        {title: "Parking Place",
-                        "data": null,
-                        "render": function ( data, type, row ) {
-                                return data.city +', '+ data.street +' '+ data.streetNumber;
-                            }
-                        },
+                {
+                    "data": "id",
+                    "visible": false
+                },
+                {
+                    title: "Car Model",
+                    "data": null,
+                    render: function (data, type, row) {
+                        return data.car.producer + ' ' + data.car.model + ' ' + data.car.number;
+                    }
+                },
+                {
+                    title: "Parking Place",
+                    "data": null,
+                    "render": function (data, type, row) {
+                        return data.city + ', ' + data.street + ' ' + data.streetNumber;
+                    }
+                },
 
-                        {title: "Data Time Start",
-                        "data": "dateTimeStart" },
+                {
+                    title: "Data Time Start",
+                    "data": "dateTimeStart"
+                },
 
-                        {title: "Data Time Start",
-                         "data": "dateTimeEnd" }
-                    ],
-                    "language": {
-                                    "sEmptyTable":     "No data available in table",
-                                        "sInfo":           "Showing _START_ to _END_ of _TOTAL_ entries",
-                                        "sInfoEmpty":      "Showing 0 to 0 of 0 entries",
-                                        "sInfoFiltered":   "(filtered from _MAX_ total entries)",
-                                        "sInfoPostFix":    "",
-                                        "sInfoThousands":  ",",
-                                        "sLengthMenu":     "Show _MENU_ entries",
-                                        "sLoadingRecords": "Loading...",
-                                        "sProcessing":     "Processing...",
-                                        "sSearch":         "Search:",
-                                        "sZeroRecords":    "No matching records found",
-                                        "oPaginate": {
-                                            "sFirst":    "First",
-                                            "sLast":     "Last",
-                                            "sNext":     "Next",
-                                            "sPrevious": "Previous"
-                                        },
-                                        "oAria": {
-                                            "sSortAscending":  ": activate to sort column ascending",
-                                            "sSortDescending": ": activate to sort column descending"
-                                        }
-                                }
+                {
+                    title: "Data Time Start",
+                    "data": "dateTimeEnd"
+                }
+            ],
+            "language": {
+                "sEmptyTable": "No data available in table",
+                "sInfo": "Showing _START_ to _END_ of _TOTAL_ entries",
+                "sInfoEmpty": "Showing 0 to 0 of 0 entries",
+                "sInfoFiltered": "(filtered from _MAX_ total entries)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ",",
+                "sLengthMenu": "Show _MENU_ entries",
+                "sLoadingRecords": "Loading...",
+                "sProcessing": "Processing...",
+                "sSearch": "Search:",
+                "sZeroRecords": "No matching records found",
+                "oPaginate": {
+                    "sFirst": "First",
+                    "sLast": "Last",
+                    "sNext": "Next",
+                    "sPrevious": "Previous"
+                },
+                "oAria": {
+                    "sSortAscending": ": activate to sort column ascending",
+                    "sSortDescending": ": activate to sort column descending"
+                }
+            }
 
-         }
-     );
-
-
+        }
+    );
 
 
     var platform = new H.service.Platform({
-       'app_id': 'IMKvUNuOHPm9m6STg3ax',
-       'app_code': 'nFMkNnKiTHe_BkWaKjI_JA'
-     });
+        'app_id': 'IMKvUNuOHPm9m6STg3ax',
+        'app_code': 'nFMkNnKiTHe_BkWaKjI_JA'
+    });
     var maptypes = platform.createDefaultLayers();
     map = new H.Map(
-      document.getElementById('map'),
-      maptypes.normal.map,
-      {
-        zoom: 13,
-        center: { lng:  35.018446, lat:48.474591 }
-      });
+        document.getElementById('map'),
+        maptypes.normal.map,
+        {
+            zoom: 13,
+            center: {lng: 35.018446, lat: 48.474591}
+        });
     ui = H.ui.UI.createDefault(map, maptypes);
     behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
     group = new H.map.Group();
     map.addObject(group);
-     $( '#validateFormSaveOrEdit' ).validate( {
+    $('#validateFormSaveOrEdit').validate({
         rules: {
             carNumber: {
                 required: true,
                 remote: {
                     url: basicUrl + "/uniqueNumber",
-                    dataType:'json',
-                    data:{
-                        id: function(){return $("#idNumber").val()}
+                    dataType: 'json',
+                    data: {
+                        id: function () {
+                            return $("#idNumber").val()
+                        }
                     },
                     contentType: 'application/json'
                 }
             },
-            carType: {selectTypeNotEmpty:"0"},
-            carProducer: {selectTypeNotEmpty:"0"},
-            carName: {selectTypeNotEmpty:"0"}
+            carType: {selectTypeNotEmpty: "0"},
+            carProducer: {selectTypeNotEmpty: "0"},
+            carName: {selectTypeNotEmpty: "0"}
         },
         messages: {
-            carNumber:{
+            carNumber: {
 
-              required: "Requared number",
-              remote: "notUnique car Number"
+                required: "Requared number",
+                remote: "notUnique car Number"
             }
         },
         errorElement: "em",
-        errorPlacement: function ( error, element ) {
+        errorPlacement: function (error, element) {
             // Add the `help-block` class to the error element
-            error.addClass( "help-block" );
+            error.addClass("help-block");
 
             // Add `has-feedback` class to the parent div.form-group
             // in order to add icons to inputs
-            element.parents( ".form-group" ).addClass( "has-feedback" );
-            error.insertAfter( element );
+            element.parents(".form-group").addClass("has-feedback");
+            error.insertAfter(element);
 
             // Add the span element, if doesn't exists, and apply the icon classes to it.
-            if ( !element.next( "span" )[ 0 ] ) {
-                $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+            if (!element.next("span")[0]) {
+                $("<span class='glyphicon glyphicon-remove form-control-feedback'></span>").insertAfter(element);
             }
         },
-        success: function ( label, element ) {
+        success: function (label, element) {
             // Add the span element, if doesn't exists, and apply the icon classes to it.
-            if ( !$( element ).next( "span" )[ 0 ] ) {
-                $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+            if (!$(element).next("span")[0]) {
+                $("<span class='glyphicon glyphicon-ok form-control-feedback'></span>").insertAfter($(element));
             }
         },
-        highlight: function ( element, errorClass, validClass ) {
-                            $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
-                            $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
-                        },
-        unhighlight: function ( element, errorClass, validClass ) {
-                            $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
-                            $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
-                        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).parents(".form-group").addClass("has-error").removeClass("has-success");
+            $(element).next("span").addClass("glyphicon-remove").removeClass("glyphicon-ok");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parents(".form-group").addClass("has-success").removeClass("has-error");
+            $(element).next("span").addClass("glyphicon-ok").removeClass("glyphicon-remove");
+        },
         submitHandler: function () {
             saveOrEditCar();
-         }
+        }
     });
 });
 
-$.validator.addMethod("selectTypeNotEmpty", function(value, element,arg){
-  return arg !== value;
- }, "Value must not be empty arg.");
+$.validator.addMethod("selectTypeNotEmpty", function (value, element, arg) {
+    return arg !== value;
+}, "Value must not be empty arg.");
 
